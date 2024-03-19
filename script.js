07f67beed7d4a3a6fd778750c96316cb6ce99e59
Vincent Prévost <vincentprevost@live.fr>
2024-03-19 16:52:50 +0100

    ajout d'api dans le script

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const apiKey = "0d25d8930adb2107b9b213b3a14dbc25";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=paris";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}l`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 
+
+    console.log(Math.round(data.main.temp) + "°C")
 }
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
 
 checkWeather();
