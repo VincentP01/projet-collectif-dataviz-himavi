18695dece9c86beac28fc5b3f12a484e74f5c9c6
Vincent Prévost <vincentprevost@live.fr>
2024-03-19 16:19:04 +0100

    script modifié

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const apiKey = "0d25d8930adb2107b9b213b3a14dbc25";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=paris";
 
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}l`);
+    let data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
+
+}
+
+checkWeather();
