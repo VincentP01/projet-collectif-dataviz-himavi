fb517a510b857cf2ae4f8460b48cb318ac8c812b
Vincent Prévost <vincentprevost@live.fr>
2024-03-20 16:55:30 +0100

    Version touche entrée

--- script.js
+++ script.js
@@ -7,34 +7,60 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
 
-    console.log(data);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        let data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+        console.log(data);
 
-    if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Drizzle") {
-        weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-        weatherIcon.src = "images/mist.png";
-    } else if (data.weather[0].main == "Rain") {
-        weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "Snow") {
-        weatherIcon.src = "images/snow.png";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
+        }
+
+        document.querySelector(".weather").style.display = "block"
+        document.querySelector(".error").style.display = "none";
+
     }
-
-    console.log(Math.round(data.main.temp) + "°C")
 }
 
-searchBtn.addEventListener("click", () => {
+let clickButton = searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 })
+let pressTouch = searchBox.addEventListener("keydown", (event) => {
+    if (event.isComposing || event.keyCode === 13) checkWeather(searchBox.value);
+})
+
+
+// KeyboardEvent: key='Enter' | code='Enter'
+
+// if (event.type === "click" || (event.type === "keypress" && event.key === "Enter")) {
+//     // Empêcher le rechargement de la page (si le bouton est dans un formulaire)
+//     event.preventDefault();
+// }
+// jQuery(function ($) {
+//     let t = ${('#test');
+//     t.on('keyup', function (e) {
+//         if (e.keyCode == 13)
+//     });
+// });
+
 
 checkWeather()
