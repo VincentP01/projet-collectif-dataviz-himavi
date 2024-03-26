452d71ab777ea0fba921c69f66b34bdf022f8627
Vincent Prévost <vincentprevost@live.fr>
2024-03-26 10:46:01 +0100

    script.js avant test préchargement 2

--- script.js
+++ script.js
@@ -6,41 +6,11 @@ const apiKey = "0d25d8930adb2107b9b213b3a14dbc25";
 const apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-// const apiKeyCity = "YABIyIQuHQTgv1dFek4U0CA0YLzI7eCr7HhICkE75f4";
-// const apiUrlCity = "https://api.unsplash.com/search/photos/"
-
 // fonction + class
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
-// const weatherImages = [
-//     "url('./background/ClearPicture1.jpg')",
-//     "url('./background/ClearPicture2.jpg')",
-//     "url('./background/CloudsPicture1.jpg')",
-//     "url('./background/CloudsPicture2.jpg')",
-//     "url('./background/DrizzlePicture1.jpg')",
-//     "url('./background/DrizzlePicture2.jpg')",
-//     "url('./background/RainPicture1.jpg')",
-//     "url('./background/RainPicture2.jpg')",
-//     "url('./background/SnowPicture1.jpg')",
-//     "url('./background/SnowPicture2.jpg')",
-// ]
-
-// function imageInArray(min, max) {
-//     Math.floor(Math.random(weatherImages.length))
-// }
-
-// function imageInArray(min, max) {
-//     const minCeiled = Math.ceil(min);
-//     const maxFloored = Math.floor(max);
-//     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-// }
-// console.log(weatherImages[imageInArray(0, 3)])
-
-
-// const multiColorPictures = document.querySelector(".multiColor-pictures");
-// const clearBackground = (document.body.style.backgroundImage = "url(/background/RainbowPicture1.jpg)"); // url(/background/ClearPicture2.jpg)
 
 //test de verification dans la console
 console.log("hello");
@@ -70,39 +40,16 @@ async function checkWeather(city) {
 
         if (data.weather[0].main == "Clouds") {
             weatherIcon.src = "images/clouds.png";
-            // document.getElementById("background").style.backgroundImage =
-            //     weatherImages[imageInArray(2, 4)];
         } else if (data.weather[0].main == "Clear") {
             weatherIcon.src = "images/clear.png";
-            // {
-            //     document.getElementById("background").style.backgroundImage =
-            //         weatherImages[imageInArray(0, 2)];
-            // }
         } else if (data.weather[0].main == "Drizzle") {
             weatherIcon.src = "images/drizzle.png";
-            // {
-            //     document.getElementById("background").style.backgroundImage =
-            //         weatherImages[imageInArray(4, 6)];
-            // }
         } else if (data.weather[0].main == "Mist") {
             weatherIcon.src = "images/mist.png";
-            // {
-            //     document.getElementById("background").style.backgroundImage =
-            //         weatherImages[imageInArray(4, 6)];
-            // }
         } else if (data.weather[0].main == "Rain") {
             weatherIcon.src = "images/rain.png";
-
-            // {
-            //     document.getElementById("background").style.backgroundImage =
-            //         weatherImages[imageInArray(6, 8)];
-            // }
         } else if (data.weather[0].main == "Snow") {
             weatherIcon.src = "images/snow.png";
-            // {
-            //     document.getElementById("background").style.backgroundImage =
-            //         weatherImages[imageInArray(8, 10)];
-            // }
         }
 
         document.querySelector(".weather").style.display = "block";
@@ -147,6 +94,3 @@ async function displayCityBackground(city) {
         document.getElementById("background").style.backgroundImage = backgroundCity
     }
 }
-
-// displayCityBackground()
-// checkWeather();
